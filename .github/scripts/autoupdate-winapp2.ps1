ad0d06e24e578ca5d3d67ec1f95838ebe4ea5c8f
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-05-31 21:08:22 +0200

    cleanup

--- .github/scripts/autoupdate-winapp2.ps1
+++ .github/scripts/autoupdate-winapp2.ps1
@@ -3,8 +3,6 @@ $MyURL="https://content.thewebatom.net/files/winapp2.ini"
 $wc = [System.Net.WebClient]::new()
 $FileHash = Get-FileHash -InputStream ($wc.OpenRead($MyURL))
 
-$FileHash.Hash
-
 $a = Get-Content '.\bucket\ccleanerpro.json' -raw | ConvertFrom-Json
 $a.hash[1]=$FileHash.Hash
 $a | ConvertTo-Json -depth 32 | %{[Regex]::Replace($_,"\\u(?<Value>[a-zA-Z0-9]{4})", {param($m) ([char]([int]::Parse($m.Groups['Value'].Value, [System.Globalization.NumberStyles]::HexNumber))).ToString() } )} |set-content '.\bucket\ccleanerpro.json'
